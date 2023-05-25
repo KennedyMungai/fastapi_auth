ac99bf1f53cd9711e91bd9dbec97d57514d55091
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-25 15:03:58 +0300

    Added a bunch of docstrings

--- models/user_model.py
+++ models/user_model.py
@@ -3,11 +3,17 @@ from pydantic import BaseModel, Field, EmailStr
 
 
 class UserSchema(BaseModel):
+    """The model for the user
+
+    Args:
+        BaseModel (Pydantic): The parent class for the User Model
+    """
     fullname: str = Field(default=None)
     email: EmailStr = Field(default=None)
     password: str = Field(default=None)
 
     class Config:
+        """The config file for the class """
         the_schema = {
             "user_demo": {
                 "name": "Somebody's Name",
