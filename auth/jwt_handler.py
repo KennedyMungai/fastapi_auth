f9c89002863cbe6b5cfeb2bd255a3fa4c360467b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-25 14:42:28 +0300

    Loaded all the environment variables

--- auth/jwt_handler.py
+++ auth/jwt_handler.py
@@ -1,4 +1,5 @@
 """The file that handles the JWT i.e. signing, encoding, decoding and returning JWTs"""
+import os
 from time import datetime
 
 import jwt
@@ -6,3 +7,6 @@ from dotenv import find_dotenv, load_dotenv
 
 
 load_dotenv(find_dotenv())
+
+JWT_SECRET = os.environ.get("SECRET")
+JWT_ALGORITHM = os.environ.get("ALGORITHM")
