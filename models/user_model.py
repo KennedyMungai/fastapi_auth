af83176a045a6bc88447d83b8525e1ddbae6e6e1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-25 15:03:09 +0300

    Created the model file for the User

--- models/user_model.py
+++ models/user_model.py
@@ -1,1 +1,17 @@
 """Created the Model file for the User"""
+from pydantic import BaseModel, Field, EmailStr
+
+
+class UserSchema(BaseModel):
+    fullname: str = Field(default=None)
+    email: EmailStr = Field(default=None)
+    password: str = Field(default=None)
+
+    class Config:
+        the_schema = {
+            "user_demo": {
+                "name": "Somebody's Name",
+                "email": "chicken@wings.com",
+                "password": "password"
+            }
+        }
