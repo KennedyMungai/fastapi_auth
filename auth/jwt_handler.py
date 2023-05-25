c596b0fdcc27d6175cb94ebc67c2c1f3ca80d044
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-25 14:44:33 +0300

    Created a function that returns an access token

--- auth/jwt_handler.py
+++ auth/jwt_handler.py
@@ -10,3 +10,17 @@ load_dotenv(find_dotenv())
 
 JWT_SECRET = os.environ.get("SECRET")
 JWT_ALGORITHM = os.environ.get("ALGORITHM")
+
+
+def token_response(token: str):
+    """The function that returns a generated token
+
+    Args:
+        token (str): The JWT token
+
+    Returns:
+        dict: access token
+    """
+    return {
+        "access_token": token
+    }
