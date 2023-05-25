f6797b60281e3d2593b72bbc5d161bb184ffa72c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-25 15:26:48 +0300

    Defined some code

--- auth/jwt_bearer.py
+++ auth/jwt_bearer.py
@@ -1,1 +1,11 @@
 """The file which contains the bearer logic"""
+from fastapi import Request, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+
+class jwtBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(jwtBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(jwtBearer, self).__call__(request)
