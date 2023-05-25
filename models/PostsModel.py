f40a562153891658586fe81ddf3ca01ac4ad7ed8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-25 13:57:04 +0300

    Added some docstrings

--- models/PostsModel.py
+++ models/PostsModel.py
@@ -3,11 +3,17 @@ from pydantic import BaseModel, Field, EmailStr
 
 
 class PostsSchema(BaseModel):
+    """The template for the posts data
+
+    Args:
+        BaseModel (Pydantic): The parent for the Posts
+    """
     id: int = Field(default=None)
     title: str = Field(default=None)
     content: str = Field(default=None)
 
     class Config:
+        """The config for the Posts model"""
         schema_extra = {
             "post_demo": {
                 "title": "some title about animals",
